0236cd7dcb2d67ca078dbd6c17fdbfc4c1a27dff
Alemorar <infoserv100@gmail.com>
2015-11-10 23:35:39 -0300

    calculadora funcionando2

--- app/src/main/java/com/example/nahuel/calcbasica/MainActivity.java
+++ app/src/main/java/com/example/nahuel/calcbasica/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.nahuel.calcbasica;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.*;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity  implements OnClickListener{
 
@@ -20,24 +16,30 @@ public class MainActivity extends AppCompatActivity  implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View boton = findViewById(R.id.btnCalcular);
+        View boton = findViewById(R.id.btnCalcularSuma);
         boton.setOnClickListener(this);
 
+        View boton2 = findViewById(R.id.btnCalcularResta);
+        boton2.setOnClickListener(this);
+
+        View boton3 = findViewById(R.id.btnCalcularProducto);
+        boton3.setOnClickListener(this);
+
+        View boton4 = findViewById(R.id.btnCalcularCociente);
+        boton4.setOnClickListener(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,8 +72,26 @@ public class MainActivity extends AppCompatActivity  implements OnClickListener{
             float a = Float.parseFloat(temp.getText().toString());
             temp = (TextView) findViewById(R.id.editText2);
             float b = Float.parseFloat(temp.getText().toString());
-            resultado.setText("resultado : " + (a + b));
-        } catch (NumberFormatException e) {
+
+            switch (v.getId()){
+                case R.id.btnCalcularSuma:
+                    resultado.setText("resultado suma : " + (a + b));
+                    break;
+                case R.id.btnCalcularResta:
+                    resultado.setText("resultado resta : " + (a - b));
+                    break;
+                case R.id.btnCalcularProducto:
+                    resultado.setText("resultado producto : " + (a * b));
+                    break;
+                case R.id.btnCalcularCociente:
+                    try {
+                        resultado.setText("resultado cociente : " + (float)(a / b));
+                    } catch (ArithmeticException ex){
+                        resultado.setText("El divisor debe ser mayor a 0");
+                    }
+                    break;
+            }
+        } catch (NumberFormatException nfe) {
             resultado.setText("Se deben introducir solo numeros");
         }
     }
